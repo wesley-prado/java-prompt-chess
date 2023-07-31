bd967780ea0263e67e9748ce225b0d71ff0f22ce
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2023-07-30 23:02:43 -0300

    feat(board): BoardException & defensive programming

--- codemages/src/boardgame/Board.java
+++ codemages/src/boardgame/Board.java
@@ -6,6 +6,10 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1)
+			throw new BoardException(
+					"Error creating board: there must be at least 1 row and 1 column");
+
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -27,7 +31,15 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
+	public void placePiece(Piece piece, int row, int column) {
+		placePiece(piece, new Position(row, column));
+	}
+
 	public void placePiece(Piece piece, Position position) {
+		if (isThereAPiece(position))
+			throw new BoardException(
+					"There is already a piece on position " + position);
+
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
@@ -36,5 +48,25 @@ public class Board {
 		Piece piece = getPiece(position);
 		pieces[position.getRow()][position.getColumn()] = null;
 		return piece;
+	}
+
+	public boolean isThereAPiece(int row, int column) {
+		return isThereAPiece(new Position(row, column));
+	}
+
+	public boolean isThereAPiece(Position position) {
+		if (!isPositionValid(position))
+			throw new BoardException("Position not on the board: " + position);
+
+		return getPiece(position) != null;
+	}
+
+	public boolean isPositionValid(int row, int column) {
+		return isPositionValid(new Position(row, column));
+	}
+
+	public boolean isPositionValid(Position position) {
+		return position.getRow() >= 0 && position.getRow() < rows
+				&& position.getColumn() >= 0 && position.getColumn() < columns;
 	}
 }
