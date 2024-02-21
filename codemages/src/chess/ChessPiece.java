fb152ab62a765d0d6fe45279b5b3cf31d3d239c6
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2024-02-20 22:16:26 -0300

    feat: add isThereOpponentPiece method

--- codemages/src/chess/ChessPiece.java
+++ codemages/src/chess/ChessPiece.java
@@ -1,17 +1,23 @@
 package chess;
 
 import boardgame.Piece;
+import boardgame.Position;
 import boardgame.Board;
 
 public abstract class ChessPiece extends Piece {
 	private Color color;
 
-	public ChessPiece(Board board, Color color) {
+	protected ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
 
 	public Color getColor() {
 		return color;
+	}
+
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().getPiece(position);
+		return piece != null && piece.getColor() != this.color;
 	}
 }
