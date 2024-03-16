9d0371a9d9a9654ed6efdf2d83ab9462b256d01a
Wesley Prado <55308332+wesley-prado@users.noreply.github.com>
2024-03-15 23:25:37 -0300

    feat: add bishop movement logic

--- codemages/src/chess/pieces/Bishop.java
+++ codemages/src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,6 +20,77 @@ public class Bishop extends ChessPiece {
 		Board board = getBoard();
 		boolean[][] matrix = new boolean[board.getRows()][board.getColumns()];
 
+		checkUpLeftSquares(matrix);
+		checkUpRightSquares(matrix);
+		checkDownLeftSquares(matrix);
+		checkDownRightSquares(matrix);
+
 		return matrix;
+	}
+
+	private void checkUpLeftSquares(boolean[][] matrix) {
+		Position p = new Position(0, 0);
+
+		int row = this.position.getRow() - 1;
+		int column = this.position.getColumn() - 1;
+		p.setValues(row, column);
+
+		while (this.getBoard().isPositionValid(p) && !this.getBoard().isThereAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(--row, --column);
+		}
+
+		checkCapturablePiece(matrix, p);
+	}
+
+	private void checkUpRightSquares(boolean[][] matrix) {
+		Position p = new Position(0, 0);
+
+		int row = this.position.getRow() - 1;
+		int column = this.position.getColumn() + 1;
+		p.setValues(row, column);
+
+		while (this.getBoard().isPositionValid(p) && !this.getBoard().isThereAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(--row, ++column);
+		}
+
+		checkCapturablePiece(matrix, p);
+	}
+
+	private void checkDownLeftSquares(boolean[][] matrix) {
+		Position p = new Position(0, 0);
+
+		int row = this.position.getRow() + 1;
+		int column = this.position.getColumn() - 1;
+		p.setValues(row, column);
+
+		while (this.getBoard().isPositionValid(p) && !this.getBoard().isThereAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(++row, --column);
+		}
+
+		checkCapturablePiece(matrix, p);
+	}
+
+	private void checkDownRightSquares(boolean[][] matrix) {
+		Position p = new Position(0, 0);
+
+		int row = this.position.getRow() + 1;
+		int column = this.position.getColumn() + 1;
+		p.setValues(row, column);
+
+		while (this.getBoard().isPositionValid(p) && !this.getBoard().isThereAPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(++row, ++column);
+		}
+
+		checkCapturablePiece(matrix, p);
+	}
+
+	private void checkCapturablePiece(boolean[][] matrix, Position position) {
+		if (getBoard().isPositionValid(position) && isThereOpponentPiece(position)) {
+			matrix[position.getRow()][position.getColumn()] = true;
+		}
 	}
 }
